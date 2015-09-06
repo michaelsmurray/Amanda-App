0aaf0b66f40bdc69f55db8ed9b9cb6ace1ceb03b
michaelsmurray <michaelsmurray6@gmail.com>
2015-09-06 16:35:11 -0500

    App user interface

---
+++ ui.R
@@ -0,0 +1,25 @@
+## Example UI
+
+fluidPage(
+  # Application title
+  titlePanel("Word Cloud"),
+  
+  sidebarLayout(
+    # Sidebar with a slider and selection inputs
+    sidebarPanel(
+      actionButton("update", "Change"),
+      hr(),
+      sliderInput("freq",
+                  "Minimum Frequency:",
+                  min = 1,  max = 50, value = 15),
+      sliderInput("max",
+                  "Maximum Number of Words:",
+                  min = 1,  max = 300,  value = 100)
+    ),
+    
+    # Show Word Cloud
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
